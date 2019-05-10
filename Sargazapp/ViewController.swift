fcf24295ebe2625e33b3f46879489af39ea1363d
rudemigue <miguel_jimenezc@icloud.com>
2019-05-10 09:29:26 -0500

    Initial Commit

---
+++ Sargazapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sargazapp
+//
+//  Created by Miguel Jiménez on 5/10/19.
+//  Copyright © 2019 Sargazapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
